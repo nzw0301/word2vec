6ec33a27a5187612e1c72c7d864a42a0f4f9c71e
NOZAWA Kento <k_nzw@klis.tsukuba.ac.jp>
2019-10-31 14:48:31 +0900

    Do not save </s> vector

--- word2vec.c
+++ word2vec.c
@@ -569,10 +569,11 @@ void TrainModel() {
   start = clock();
   for (a = 0; a < num_threads; a++) pthread_create(&pt[a], NULL, TrainModelThread, (void *)a);
   for (a = 0; a < num_threads; a++) pthread_join(pt[a], NULL);
+  
   fo = fopen(output_file, "wb");
-    // Save the word vectors
-  fprintf(fo, "%lld %lld\n", vocab_size, layer1_size);
-  for (a = 0; a < vocab_size; a++) {
+  // Save the word vectors
+  fprintf(fo, "%lld %lld\n", vocab_size-1, layer1_size);
+  for (a = 1; a < vocab_size; a++) {
     fprintf(fo, "%s ", vocab[a].word);
     if (binary) for (b = 0; b < layer1_size; b++) fwrite(&syn0[a * layer1_size + b], sizeof(real), 1, fo);
     else for (b = 0; b < layer1_size; b++) fprintf(fo, "%lf ", syn0[a * layer1_size + b]);
@@ -584,8 +585,8 @@ void TrainModel() {
   char *context_output_file = strcat(prefix, output_file);
   fo = fopen(context_output_file, "wb");
   // Save the context word vectors
-  fprintf(fo, "%lld %lld\n", vocab_size, layer1_size);
-  for (a = 0; a < vocab_size; a++) {
+  fprintf(fo, "%lld %lld\n", vocab_size-1, layer1_size);
+  for (a = 1; a < vocab_size; a++) {
     fprintf(fo, "%s ", vocab[a].word);
     if (binary) for (b = 0; b < layer1_size; b++) fwrite(&syn1neg[a * layer1_size + b], sizeof(real), 1, fo);
     else for (b = 0; b < layer1_size; b++) fprintf(fo, "%lf ", syn1neg[a * layer1_size + b]);
